e5dee40bfed89673134235b36d6ecf7a8f04e59f
cojcoj <support-032751@learnerstechlab.com>
2015-06-04 16:25:57 +0900

    Fix TrackBrowse for Oneshot

--- myMusicPlayer21/src/main/java/com/kojimahome/music21/TrackBrowserActivity.java
+++ myMusicPlayer21/src/main/java/com/kojimahome/music21/TrackBrowserActivity.java
@@ -315,6 +315,7 @@ public class TrackBrowserActivity extends ListActivity
         if (mTrackCursor != null) {
             getListView().invalidateViews();
         }
+        MusicUtils.updateNowPlaying(TrackBrowserActivity.this);
         MusicUtils.setSpinnerState(this);
     }
     @Override
